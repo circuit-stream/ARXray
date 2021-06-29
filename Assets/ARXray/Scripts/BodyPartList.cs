e24e3bf07303fbc8cb78b5663a960a8b034dfa67
Arthur Ribeiro <arthhur.ribeiro@gmail.com>
2021-06-29 16:23:46 -0300

    Refactor BodyPartList into a single function

--- Assets/ARXray/Scripts/BodyPartList.cs
+++ Assets/ARXray/Scripts/BodyPartList.cs
@@ -3,52 +3,8 @@ using UnityEngine.UI;
 
 public class BodyPartList : MonoBehaviour
 {
-	public Image brainIcon;
-	public Image heartIcon;
-	public Image leftLungIcon;
-	public Image rightLungIcon;
-	public Image stomachIcon;
-	public Image leftKidneyIcon;
-	public Image rightKidneyIcon;
-	public Image bladderIcon;
-
-	public void OnBrainFound()
+	public void OnBodyPartClicked(Image bodyPart)
 	{
-		brainIcon.color = new Color(1, 1, 1, 1);
-	}
-	
-	public void OnHeartFound()
-	{
-		heartIcon.color = new Color(1, 1, 1, 1);
-	}
-	
-	public void OnLeftLungFound()
-	{
-		leftLungIcon.color = new Color(1, 1, 1, 1);
-	}
-	
-	public void OnRightLungFound()
-	{
-		rightLungIcon.color = new Color(1, 1, 1, 1);
-	}
-	
-	public void OnStomachFound()
-	{
-		stomachIcon.color = new Color(1, 1, 1, 1);
-	}
-	
-	public void OnLeftKidneyFound()
-	{
-		leftKidneyIcon.color = new Color(1, 1, 1, 1);
-	}
-	
-	public void OnRightKidneyFound()
-	{
-		rightKidneyIcon.color = new Color(1, 1, 1, 1);
-	}
-	
-	public void OnBladderFound()
-	{
-		bladderIcon.color = new Color(1, 1, 1, 1);
+		bodyPart.color = Color.white;
 	}
 }
